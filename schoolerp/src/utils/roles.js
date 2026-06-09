976485fe8b674fba2d2477522bce6cc4cff6f068
devashishsonkusare <devsonkusare13@gmail.com>
2026-06-10 00:51:36 +0530

    feat: add canManageSubjects role helper

--- schoolerp/src/utils/roles.js
+++ schoolerp/src/utils/roles.js
@@ -45,3 +45,7 @@ export function canManageStandards(user) {
 export function canManageSections(user) {
   return isSuperAdmin(user);
 }
+
+export function canManageSubjects(user) {
+  return isSuperAdmin(user) || isPrincipal(user);
+}
